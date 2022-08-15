37e91c151bc541ba2a335a68e3b63c42cb868e11
cihanbahcivan <cihanbhcvn@gmail.com>
2022-08-16 00:08:26 +0300

    1. Ders sonu

---
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+
+        public BaseDbContext(DbContextOptions options, IConfiguration configuration ) : base(options)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("RentACarCampConnectionString"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(a =>
+            {
+                a.ToTable("Brands").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
+            modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+        }
+    }
+}
